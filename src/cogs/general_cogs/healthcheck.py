d46245cf7c331abce913d82fb6f3e98de732926a
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2024-05-14 20:44:42 +0200

    Updated ping to a prefix command. Just use ">ping"

--- src/cogs/general_cogs/healthcheck.py
+++ src/cogs/general_cogs/healthcheck.py
@@ -2,8 +2,6 @@ import logging
 import discord
 from discord.ext import commands
 
-
-
 # We add our logger to the cog.
 logger = logging.getLogger(__name__)
 
@@ -14,17 +12,18 @@ class Healthcheck(discord.ext.commands.Cog):
     If an error occurs in the cog, the cog itself will fail and restart.
     It will NOT bring down the bot with it.
     """
+
     def __init__(self, bot: commands.Bot) -> None:
         """ Initialize the class using the bot object from __main__ """
         self.bot = bot
 
-    @discord.app_commands.command(name="ping")
-    async def ping(self, interaction: discord.Interaction):
-        logger.info(f"ping command used by {interaction.user.name}")
+    @commands.command(name="ping")
+    async def ping(self, ctx):
+        logger.info(f"ping command used by {ctx.author.name}")
 
         shit_speed = f"{str(round(self.bot.latency * 1000))} ms"
-        embed = discord.Embed(title="**Pong!**", description="**" + shit_speed + "**", color=0xafdafc)
-        await interaction.response.send_message(embed=embed)
+        embed = discord.Embed(title="**Pong!**", description=f"**{shit_speed}**", color=0xafdafc)
+        await ctx.reply(embed=embed)
 
 
 async def setup(bot: commands.Bot) -> None:
